366d018290be51d09708b260a20c220593390fa3
Xiangtao Wu <appleafter321@gmail.com>
2026-07-13 15:41:53 -0400

    general layout 0.5v

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,37 @@
 export default function Home() {
   return (
     <main>
-      <h1>Weather Dashboard</h1>
+      <aside className="sidebar">Side bar</aside>
+
+      <section className="weather-panel">
+        {/*Logo*/}
+        <h1>Weatherly</h1>
+      </section>
+
+      {/*Main content*/}
+      <section className="forecast-area">
+        <header className="header">
+          <input className="search-bar"></input>
+
+          <div className="menu">Settings</div>
+        </header>
+
+        {/*Hero weather*/}
+        <div className="hero-weather">Hero</div>
+
+        <div className="hourly-forecast">Hourly forecast</div>
+
+        <section className="bottom-cards">
+          {/*Weekly forecast*/}
+          <div className="weekly-forecast">Weekly</div>
+
+          <div className="precipitation">Precipitation</div>
+          
+          <div className="sun-moon">Sun & Moon</div>
+        </section>
+      </section>
+
+
     </main>
   );
 }
